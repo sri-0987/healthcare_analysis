1c992372c0e703926b7678a549cbef08568eca99
Srilakshmi <yaraguni.srilakshmi@capgemini.com>
2026-05-25 10:44:54 +0530

    updated readme file

--- api.py
+++ api.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI # type: ignore
 from database import get_connection
 import psycopg2 # type: ignore
+from pydantic import BaseModel # type: ignore
 
 app = FastAPI()
 
@@ -20,45 +21,15 @@ def get_patients():
 
 
 # ✅ GET API - Fetch doctors
-@app.get("/doctors")
-def get_doctors():
-    conn = get_connection()
-    cursor = conn.cursor()
+class PatientRequest(BaseModel):
+    patient_id: int
+    name: str
+    age: int
+    gender: str
+    city: str
 
-    cursor.execute("SELECT * FROM dim_doctors LIMIT 10;")
-    rows = cursor.fetchall()
-
-    cursor.close()
-    conn.close()
-
-    return {"data": rows}
-
-
-# ✅ GET API - Join query (important for POC)
-@app.get("/visits")
-def get_visits():
-    conn = get_connection()
-    cursor = conn.cursor()
-
-    cursor.execute("""
-        SELECT p.name, d.name, f.disease, f.treatment_cost
-        FROM fact_patient_visits f
-        JOIN dim_patients p ON f.patient_id = p.patient_id
-        JOIN dim_doctors d ON f.doctor_id = d.doctor_id
-        LIMIT 10;
-    """)
-
-    rows = cursor.fetchall()
-
-    cursor.close()
-    conn.close()
-
-    return {"data": rows}
-
-
-# ✅ POST API - Add patient
 @app.post("/add-patient")
-def add_patient(patient_id: int, name: str, age: int, gender: str, city: str):
+def add_patient(patient: PatientRequest):
     conn = get_connection()
     cursor = conn.cursor()
 
@@ -66,10 +37,15 @@ def add_patient(patient_id: int, name: str, age: int, gender: str, city: str):
         INSERT INTO dim_patients (patient_id, name, age, gender, city)
         VALUES (%s, %s, %s, %s, %s)
         ON CONFLICT (patient_id) DO NOTHING;
-    """, (patient_id, name, age, gender, city))
+    """, (
+        patient.patient_id,
+        patient.name,
+        patient.age,
+        patient.gender,
+        patient.city
+    ))
 
     conn.commit()
-
     cursor.close()
     conn.close()
 
